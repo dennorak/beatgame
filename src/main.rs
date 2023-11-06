69aa9ed1a372f359f653c75dc582bcbda1012b2a
Mason <mason@lgtspd.net>
2023-11-06 11:59:37 -0500

    added animation, cleaned up, fmt

--- src/main.rs
+++ src/main.rs
@@ -26,23 +26,58 @@ struct RightIndicator;
 #[derive(Component)]
 struct MovingNote;
 
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::BLACK))
+        // .insert_resource(ClearColor(Color::BLACK))
         .add_systems(Startup, setup)
         .add_systems(Update, keyboard_input)
+        .add_systems(Update, unlight_markers)
         .add_systems(FixedUpdate, stream_keys)
-        .insert_resource(Time::<Fixed>::from_seconds(0.016)) 
+        .add_systems(FixedUpdate, prune_keys)
+        .insert_resource(Time::<Fixed>::from_seconds(0.008))
         .run();
+}
+
+fn create_anim(label: &Name, x: f32) -> AnimationClip {
+    let mut animation = AnimationClip::default();
+    // A curve can modify a single part of a transform, here the translation
+    animation.add_curve_to_path(
+        EntityPath {
+            parts: vec![label.clone()],
+        },
+        VariableCurve {
+            keyframe_timestamps: vec![0.0, 0.125, 0.25],
+            keyframes: Keyframes::Translation(vec![
+                Vec3::new(x, -0.125, 0.0),
+                Vec3::new(x, 0.0, 0.0),
+                // loop back
+                Vec3::new(x, -0.125, 0.0),
+            ]),
+        },
+    );
+    return animation;
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut animations: ResMut<Assets<AnimationClip>>,
 ) {
+    // name labels for amin
+    let left = Name::new("left");
+    let right = Name::new("right");
+
+    // right anim
+    let r_anim = create_anim(&right, 0.5);
+    let mut r_player = AnimationPlayer::default();
+    r_player.play(animations.add(r_anim));
+
+    // left anim
+    let l_anim = create_anim(&left, -0.5);
+    let mut l_player = AnimationPlayer::default();
+    l_player.play(animations.add(l_anim));
 
     // cam
     commands.spawn(Camera3dBundle {
@@ -58,69 +93,93 @@ fn setup(
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(0.0, 8.0, 4.0),
+        transform: Transform::from_xyz(0.0, 8.0, 4.0)
+            .looking_at(Vec3::new(0.0, 0.0, -10.0), Vec3::Y),
         ..default()
     });
 
+    let rail_mesh = meshes.add(Mesh::from(shape::Box::from_corners(
+        Vec3::new(0.0625, 0.05, 1.0),
+        Vec3::new(-0.0625, 0.0, -100.0),
+    )));
+    let rail_mat = materials.add(Color::rgb(0.0, 0.0, 0.0).into());
+
+    let marker_mesh = meshes.add(Mesh::from(shape::Box::from_corners(
+        Vec3::new(-0.375, 0.0, -0.375),
+        Vec3::new(0.375, 0.125, 0.375),
+    )));
+
     // board
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(1.0, 0.0, 1.0), Vec3::new(-1.0, 0.0, -100.0)))),
-            material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
-            ..default()
-        },
-    ));
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Box::from_corners(
+            Vec3::new(1.0, 0.0, 1.0),
+            Vec3::new(-1.0, 0.0, -100.0),
+        ))),
+        material: materials.add(Color::rgb(0.3, 0.3, 0.3).into()),
+        ..default()
+    },));
 
     // center rail
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(0.0625, 0.05, 1.0), Vec3::new(-0.0625, 0.0, -100.0)))),
-            material: materials.add(Color::rgb(0.0, 0.0, 0.0).into()),
-            ..default()
-        },
-    ));
+    commands.spawn((PbrBundle {
+        mesh: rail_mesh.clone(),
+        material: rail_mat.clone(),
+        ..default()
+    },));
 
     // left rail
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(0.0625, 0.05, 1.0), Vec3::new(-0.0625, 0.0, -100.0)))),
-            material: materials.add(Color::rgb(0.0, 0.0, 0.0).into()),
-            transform: Transform::from_xyz(-1.0, 0.0, 0.0),
-            ..default()
-        },
-    ));
+    commands.spawn((PbrBundle {
+        mesh: rail_mesh.clone(),
+        material: rail_mat.clone(),
+        transform: Transform::from_xyz(-1.0, 0.0, 0.0),
+        ..default()
+    },));
 
     // right rail
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(0.0625, 0.05, 1.0), Vec3::new(-0.0625, 0.0, -100.0)))),
-            material: materials.add(Color::rgb(0.0, 0.0, 0.0).into()),
-            transform: Transform::from_xyz(1.0, 0.0, 0.0),
-            ..default()
-        },
-    ));
+    commands.spawn((PbrBundle {
+        mesh: rail_mesh.clone(),
+        material: rail_mat.clone(),
+        transform: Transform::from_xyz(1.0, 0.0, 0.0),
+        ..default()
+    },));
 
     // right marker
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(-0.375, 0.0, -0.375), Vec3::new(0.375, 0.125, 0.375)))),
+            mesh: marker_mesh.clone(),
             material: materials.add(Color::rgba(0.78, 0.36, 0.36, 0.5).into()),
-            transform: Transform::from_xyz(0.5, 0.0, 0.0),
+            transform: Transform::from_xyz(0.5, -0.125, 0.0),
             ..default()
         },
-        RightIndicator
+        r_player,
+        right,
+        RightIndicator,
     ));
 
     // left marker
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(-0.375, 0.0, -0.375), Vec3::new(0.375, 0.125, 0.375)))),
+            mesh: marker_mesh.clone(),
             material: materials.add(Color::rgba(0.36, 0.36, 0.78, 0.5).into()),
-            transform: Transform::from_xyz(-0.5, 0.0, 0.0),
+            transform: Transform::from_xyz(-0.5, -0.125, 0.0),
             ..default()
         },
-        LeftIndicator
+        l_player,
+        left,
+        LeftIndicator,
     ));
+}
+
+fn unlight_markers(
+    marks: Query<(&Handle<StandardMaterial>, &AnimationPlayer)>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    marks.for_each(|(mat, anim)| {
+        if anim.is_finished() {
+            if let Some(mat) = materials.get_mut(mat) {
+                mat.base_color.set_a(0.5);
+            }
+        }
+    });
 }
 
 fn keyboard_input(
@@ -128,57 +187,66 @@ fn keyboard_input(
     mut meshes: ResMut<Assets<Mesh>>,
     keys: Res<Input<KeyCode>>,
     mut set: ParamSet<(
-        Query<&mut Handle<StandardMaterial>, With<LeftIndicator>>,
-        Query<&mut Handle<StandardMaterial>, With<RightIndicator>>,
+        Query<(&Handle<StandardMaterial>, &mut AnimationPlayer), With<LeftIndicator>>,
+        Query<(&Handle<StandardMaterial>, &mut AnimationPlayer), With<RightIndicator>>,
     )>,
-    mut materials: ResMut<Assets<StandardMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     if keys.just_pressed(KeyCode::F) {
-        info!("left");
-        if let Ok(handle) = set.p0().get_single()
-        {
-            if let Some(mat) = materials.get_mut(handle)
-            {
+        if let Ok((handle, mut anim)) = set.p0().get_single_mut() {
+            if let Some(mat) = materials.get_mut(handle) {
                 mat.base_color.set_a(1.0);
-            } else { error!("Failed to get mat") }
-        } else { error!("Failed to get handle") }
+            }
+            anim.replay();
+        }
 
         commands.spawn((
             PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(-0.375, 0.0, -0.375), Vec3::new(0.375, 0.125, 0.375)))),
+                mesh: meshes.add(Mesh::from(shape::Box::from_corners(
+                    Vec3::new(-0.375, 0.0, -0.375),
+                    Vec3::new(0.375, 0.125, 0.375),
+                ))),
                 material: materials.add(Color::rgba(0.36, 0.36, 0.78, 0.5).into()),
                 transform: Transform::from_xyz(-0.5, 0.0, 0.0),
                 ..default()
             },
-            MovingNote
+            MovingNote,
         ));
     }
 
     if keys.just_pressed(KeyCode::J) {
-        info!("right");
-        if let Ok(handle) = set.p1().get_single()
-        {
-            if let Some(mat) = materials.get_mut(handle)
-            {
+        if let Ok((handle, mut anim)) = set.p1().get_single_mut() {
+            if let Some(mat) = materials.get_mut(handle) {
                 mat.base_color.set_a(1.0);
-            } else { error!("Failed to get mat") }
-        } else { error!("Failed to get handle") }
+            }
+            anim.replay();
+        }
 
         commands.spawn((
             PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Box::from_corners(Vec3::new(-0.375, 0.0, -0.375), Vec3::new(0.375, 0.125, 0.375)))),
+                mesh: meshes.add(Mesh::from(shape::Box::from_corners(
+                    Vec3::new(-0.375, 0.0, -0.375),
+                    Vec3::new(0.375, 0.125, 0.375),
+                ))),
                 material: materials.add(Color::rgba(0.78, 0.36, 0.36, 0.5).into()),
                 transform: Transform::from_xyz(0.5, 0.0, 0.0),
                 ..default()
             },
-            MovingNote
+            MovingNote,
         ));
     }
 }
 
-fn stream_keys(mut movingnotes: Query<&mut Transform, With<MovingNote>>)
-{
+fn stream_keys(mut movingnotes: Query<&mut Transform, With<MovingNote>>) {
     movingnotes.for_each_mut(|mut transf| {
         transf.translation.z -= 0.1;
+    })
+}
+
+fn prune_keys(mut commands: Commands, keys: Query<(Entity, &Transform)>) {
+    keys.for_each(|(entity, transf)| {
+        if transf.translation.z <= -100.0 {
+            commands.entity(entity).despawn();
+        }
     })
 }
